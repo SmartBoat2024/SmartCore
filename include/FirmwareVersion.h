086155a38cd2029fbe68e03279d75836446d5765
Chris Saunders <chris@mysmartboat.com>
2025-12-23 10:01:41 +0000

    add firmwareVersion.h to gitignore

--- include/FirmwareVersion.h
+++ include/FirmwareVersion.h
@@ -1,5 +1,5 @@
 // AUTO-GENERATED FILE — DO NOT EDIT
 #pragma once
 
-#define FW_VER        "v@0.0.3"
-#define FW_VER_FULL   "v0.0.3-11-g00fcb97-dirty"
+#define FW_VER        "v@0.0.4"
+#define FW_VER_FULL   "v0.0.4-dirty"
